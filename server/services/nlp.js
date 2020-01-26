49f1fc001efd0454daab2763a09f61dcab2f5a15
dabris <dabris16.x@gmail.com>
2020-01-25 20:01:12 -0500

    WIP

--- server/services/nlp.js
+++ server/services/nlp.js
@@ -1,4 +1,4 @@
-async function quickstart() {
+async function processString(text) {
     // Imports the Google Cloud client library
     const language = require('@google-cloud/language');
   
@@ -7,18 +7,26 @@ async function quickstart() {
   
     // The text to analyze
     const text = 'Hello, world!';
+    const features = {
+      "extractSyntax": true,
+      "extractEntities": true,
+    };
   
     const document = {
       content: text,
-      type: 'PLAIN_TEXT',
+      type: 'PLAIN_TEXT'
     };
   
     // Detects the sentiment of the text
-    const [result] = await client.analyzeEntitySentiment({document: document});
+    const [result] = await client.annotateText({document: document, features: features});
     const entities = result.entities[0];
+    const verb = result.tokens[0].partOfSpeech;
+    console.log(`verb: ${verb}`);
+    console.log(result);
+    console.log()
     console.log(`word: ${entities.name}`);
     console.log(`type: ${entities.type}`);
     console.log(`salience: ${entities.salience}`);
   }
 
-  quickstart().catch(console.error);
+  processString().catch(console.error);
