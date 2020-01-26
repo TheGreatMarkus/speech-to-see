23ca130e6a78cbfb326e48a4b7837a13ff6a9249
dabris <dabris16.x@gmail.com>
2020-01-26 00:42:59 -0500

    added shutterstock image search example

---
+++ server/services/shutterstock.js
@@ -0,0 +1,20 @@
+const sstk = require("shutterstock-api");
+require('dotenv').config({path:'../.env'});
+
+sstk.setBasicAuth(process.env.ID, process.env.SECRET);
+console.log(process.env.ID)
+const imagesApi = new sstk.ImagesApi();
+
+const queryParams = {
+  "query": "New York",
+  "sort": "popular",
+  "orientation": "horizontal"
+};
+
+imagesApi.searchImages(queryParams)
+  .then((data) => {
+    console.log(JSON.stringify(data));
+  })
+  .catch((error) => {
+    console.error(error);
+  });
