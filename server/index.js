06334cbcded448844eec578241adff6ee4ba2864
Cristian Aldea <aldea.cristian@outlook.com>
2020-01-25 19:37:28 -0500

    Setup basic nodejs server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 1337;
+const common_path = '/api';
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`);
+});
+
+app.get(`${common_path}/hello`, (req, res) => {
+    res.send('Hello World!');
+});
+
+app.post(`${common_path}/get-voice-commands`, (req, res) => {
+    console.log("get-voice-commands endpoint called");
+    console.log(req.body);
+    console.log(req.body.speech);
+
+    let nlp = require('./services/nlp');
+
+    // data = nlp.processString(req.body.speech);
+
+    res.json(req.body);
+    
+});
